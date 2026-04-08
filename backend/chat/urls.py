28de4ccd8205631a8f33b04fd54a2db27c508c6a
asrounimene-ux <asrounimene@gmailcom>
2026-04-09 01:12:18 +0200

    Update urls.py

--- backend/chat/urls.py
+++ backend/chat/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('conversations/', views.list_conversations, name='list_conversations'),
-    path('conversations/<int:id>/messages/', views.get_message, name='get_message'),
-    path('messages/send/', views.send_message, name='send_message'),
-    path('conversations/auto-open/', views.auto_open_conversation, name='auto_open_conversation')
+   path('conversations/', views.list_conversations),
+    path('conversations/<int:id>/messages/', views.get_message),
+    path('messages/send/', views.send_message),
+    path('conversations/auto-open/', views.auto_open_conversation),
 ]
 
